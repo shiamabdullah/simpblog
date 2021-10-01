3454715c1430921311b92909324d264bb32d8d18
shiam <shiamabdullah1@gmail.com>
2021-10-02 03:48:55 +0600

    added html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+def post_list(request):  
+    return render(request, 'blog/post_list.html', {})
