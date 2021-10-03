76203c75eb1cfc013623db9f7535121b1e144314
shiam <shiamabdullah1@gmail.com>
2021-10-03 15:46:16 +0600

    template extending

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.http.response import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
 import datetime
+from blog.models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your views here.
 def post_list(request):  
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    me=User.objects.get(username='shiam')
+    postsbyme = Post.objects.filter(author=me)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'postShiam' : postsbyme})
